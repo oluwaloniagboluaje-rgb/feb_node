0b8971d8c39089dd2f5cbacef1fba0ea6cd060bb
Nimi <oluwaloniagboluaje@gmail.com>
2026-03-09 23:32:04 -1000

    fix: correct requestOTP variable name and comment out unused route

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const { createUser, editUser, deleteUser,  getUser, login, getMe, verifyUser, getAllUser, requestOtp, forgotPassword, changePassword } = require('../controllers/user.controller')
+const { createUser, editUser, deleteUser,  getUser, login, getMe, verifyUser, getAllUser, forgotPassword, changePassword, requestOTP } = require('../controllers/user.controller')
 const router = express.Router()
 
 router.post('/register', createUser)
 router.patch('/edituser/:id', editUser)
 router.delete('/deleteUser/:id' , deleteUser)
 router.get('/allusers',verifyUser,getAllUser )
-router.get('/getuser/:id', getUser)
+// router.get('/getuser/:id', getUser)
 router.post('/login', login )
-router.post('/request-otp', requestOtp )
+router.post('/request-otp',requestOTP  )
 router.post('/forgot-password', forgotPassword )
 router.post('/change-password',verifyUser, changePassword )
 
